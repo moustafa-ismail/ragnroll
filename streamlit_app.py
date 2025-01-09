c29c3a21bc1fb572fe08536dbae2ea38615358bc
moustafa-ismail <moustafa.i.reda@gmail.com>
2025-01-09 22:23:08 +0200

    move feedback_functions.py to streamlit_app to avoid session error

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,21 @@ from trulens.apps.custom import TruCustomApp
 from feedback_functions import f_context_relevance
 from trulens.dashboard import streamlit as trulens_st
 from trulens.dashboard.Leaderboard import render_leaderboard
+import numpy as np
+from trulens.core import Feedback
+from trulens.core import Select
+from trulens.providers.cortex import Cortex
 
+
+snowpark_session = get_active_session()
+
+provider = Cortex(snowpark_session.connection, "mistral-large2")
+
+f_context_relevance = (
+    Feedback(provider.context_relevance, name="Context Relevance")
+    .on_input_output()
+    .aggregate(np.mean)
+)
 
 # Configuration
 NUM_CHUNKS = 3  # Number of chunks to retrieve
